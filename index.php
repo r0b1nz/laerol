294f3d96ab80f89e5d59562f1882869ad2fa26a2
Jatin Goyal <goyal1205@gmail.com>
2017-08-05 15:31:50 +0530

    Adding project structure

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Loreal</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
