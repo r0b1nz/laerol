14c39d593fba8ac7898e8ff5dd613f1cf7ef3ad5
Jatin Goyal <goyal1205@gmail.com>
2017-08-06 15:20:53 +0530

    adding more hr screens

---
+++ hr/review_cycle.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Loreal: Feedback form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+  <header>
+    <h1 class="heading choose">L'OREAL</h1>
+  </header>
+    
+  <div class="container">
+    <h3 class="heading2">Review Count is 1</h3>
+    <button name="Submit" id="submit" class="btn btn-lg btn-block" type="submit">Start New Review Cycle</button>
+  </div> <!-- /container -->
+
+</body>
+</html>
+
---
+++ hr/view_feedback.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Loreal: Feedback form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+  <header>
+    <h1 class="heading choose">L'OREAL</h1>
+  </header>
+    
+  <div class="container center_div">
+    <table class="table">
+      <thead class="thead-inverse">
+        <tr>
+          <th>#</th>
+          <th class="do_center">Designation</th>
+          <th class="do_center">Click to View Feedback</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td class="do_center">Plant Director</td>
+          <td class="do_center"><button type="button" class="btn btn-danger" value="designationOfEmployee">View</button></td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td class="do_center">Functional Head</td>
+          <td class="do_center"><button type="button" class="btn btn-danger" value="designationOfEmployee">View</button></td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td class="do_center">Resource Manager</td>
+          <td class="do_center"><button type="button" class="btn btn-danger" value="designationOfEmployee">View</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div> <!-- /container -->
+
+</body>
+</html>
+
