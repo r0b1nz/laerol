0ca85afd8f6a90ccb00e96a5e578042f4ea0f8e1
Jatin Goyal <goyal1205@gmail.com>
2017-08-06 13:16:23 +0530

    added hr screens

---
+++ hr/credentials.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Loreal: Feedback form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+  <header>
+    <h1 class="heading choose">L'OREAL</h1>
+  </header>
+    
+  <div class="container center_div">
+    <table class="table">
+      <thead class="thead-inverse">
+        <tr>
+          <th>#</th>
+          <th>Designation</th>
+          <th>Username</th>
+          <th>Password</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Plant Director</td>
+          <td>PD@Loreal</td>
+          <td>password</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Functional Head</td>
+          <td>FH@Loreal</td>
+          <td>password</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Resource Manager</td>
+          <td>RM@Loreal</td>
+          <td>password</td>
+        </tr>
+      </tbody>
+    </table>
+  </div> <!-- /container -->
+
+</body>
+</html>
+
