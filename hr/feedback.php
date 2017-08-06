004d480d777e3c3ee1b989f1c0b1c284334236e1
Jatin Goyal <goyal1205@gmail.com>
2017-08-06 16:14:49 +0530

    added al screens

---
+++ hr/feedback.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Loreal: Feedback form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+  <header>
+    <h1 class="heading choose">L'OREAL</h1>
+  </header>
+  
+  <div class="row jumbotron">
+    <div class="col-lg-12">
+      <div class="container">
+        <table class="table">
+        <thead class="thead-inverse">
+          <tr>
+            <th>#</th>
+            <th>Competency</th>
+            <th>Self</th>
+            <th>Others</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Competency 1</td>
+            <td>4.5</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Competency 2</td>
+            <td>4</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Competency 3</td>
+            <td>5</td>
+            <td>4.5</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Competency 4</td>
+            <td>4</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>Competency 5</td>
+            <td>5</td>
+            <td>5</td>
+          </tr>
+        </tbody>
+      </table>
+      </div> <!-- /container -->
+    </div>
+
+        <div class="col-lg-12">
+      <div class="container">
+        <table class="table">
+        <thead class="thead-inverse">
+          <tr>
+            <th>Aggregate</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Self</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <td>Others</td>
+            <td>3.5</td>
+          </tr>
+        </tbody>
+      </table>
+      </div> <!-- /container -->
+    </div>
+
+  </div>  
+</body>
+</html>
+
